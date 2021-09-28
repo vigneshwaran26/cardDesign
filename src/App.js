b976ca6ebc0732d5f6b8191acc5eac002166b249
vigneshwaran26 <vicky.ravi26@gmail.com>
2021-09-28 10:19:30 +0530

    cards four

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import './App.css';
 import Cardslidertwo from './card2/cardslider2';
-import Cardslider3 from './cardthree/cardslider3';
+import Cardsliderthree from './cardthree/cardslider3';
+import Cardsliderfour from './components/card4/Cardslider4';
 import Cardslider from './components/cardslider';
 
 
@@ -14,7 +15,9 @@ function App() {
       <h2 className='title'>Card design 2</h2>
       <Cardslidertwo/>
       <h2 className='title'>Card design 3</h2>
-      <Cardslider3/>
+      <Cardsliderthree/>
+      <h2 className='title'>Card design 4</h2>
+      <Cardsliderfour/>
     </div>
     
   );
