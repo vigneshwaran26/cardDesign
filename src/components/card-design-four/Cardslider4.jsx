732e49e397936f324b0a05ec30cd786d19a95f4c
vigneshwaran26 <vicky.ravi26@gmail.com>
2022-04-01 17:59:34 +0530

    font family added

---
+++ src/components/card-design-four/Cardslider4.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './card4.css'
+
+export default function CardsliderFouth() {
+    return (
+        <div className="cards-list4">
+       
+        <div className="box4">
+         <img className="img-box4" src='https://i.pinimg.com/originals/c2/6e/84/c26e847d283bcded8815ae819ab0b727.jpg'></img>
+         <p href='#' className='courses-lang4'>Tamil</p>
+         <p href='#' className='courses-list4'>Courses 17</p>
+        </div> 
+        <div className="box4">
+        <img className="img-box4" src='https://i0.wp.com/b.englishninjas.com/wp-content/uploads/2017/06/three-important-reasons-for-learning-english.jpg?fit=2566%2C1442&ssl=1'></img>
+        <p href='#' className='courses-lang4'>English</p>
+        <p href='#' className='courses-list4'>Courses 20</p>
+        </div> 
+        <div className="box4">
+        <img className="img-box4" src='https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c190cd68679475.5b800790d9c4d.jpg'></img>
+        <p href='#' className='courses-lang4'>Malayalam</p>
+        <p href='#' className='courses-list4'>Courses 14</p>
+        </div> 
+        <div className="box4">
+        <img className="img-box4" src='https://pbs.twimg.com/media/DlM-BSDWsAABr9l?format=jpg&name=small'></img>
+        <p href='#' className='courses-lang4'>Kanada</p>
+        <p href='#' className='courses-list4'>courses 15</p>
+        </div> 
+        <div className="box4">
+        <img className="img-box4" src='https://i.pinimg.com/originals/ae/3d/e7/ae3de70cec5dfbd06b6402d78b795f78.jpg'></img>
+        <p href='#' className='courses-lang4'>Bengali</p>
+        <p href='#' className='courses-list4'>courses 16</p>
+        </div> 
+        
+        
+       </div> 
+    )
+}
---
+++ src/mockdata/card-data.js
@@ -0,0 +1,58 @@
+export const languages = [
+    {
+      lookup_key: "tamil",
+      title: "Tamil",
+      sub_title: "Tamil",
+      thumbnail_url:
+        "https://i.pinimg.com/originals/c2/6e/84/c26e847d283bcded8815ae819ab0b727.jpg"
+    },
+    {
+      lookup_key: "english",
+      title: "English",
+      sub_title: "English",
+      thumbnail_url:
+        "https://i0.wp.com/b.englishninjas.com/wp-content/uploads/2017/06/three-important-reasons-for-learning-english.jpg?fit=2566%2C1442&ssl=1"
+    },
+    {
+      lookup_key: "malayalam",
+      title: "Malayalam",
+      sub_title: "Malayalam",
+      thumbnail_url:
+        "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c190cd68679475.5b800790d9c4d.jpg"
+    },
+    {
+      lookup_key: "telugu",
+      title: "Telugu",
+      sub_title: "Telugu",
+      thumbnail_url:
+        "https://assets.thehansindia.com/h-upload/2021/07/13/1088241-telugu.webp"
+    },
+    {
+      lookup_key: "kannada",
+      title: "Kannada",
+      sub_title: "Kannada",
+      thumbnail_url:
+        "https://pbs.twimg.com/media/DlM-BSDWsAABr9l?format=jpg&name=small"
+    },
+    {
+      lookup_key: "hindi",
+      title: "Hindi",
+      sub_title: "Hindi",
+      thumbnail_url:
+        "https://m.economictimes.com/thumb/msid-72136111,width-1200,height-900,resizemode-4,imgsize-361723/hindi-thnkstck.jpg"
+    },
+    {
+      lookup_key: "sanskrit",
+      title: "Sanskrit",
+      sub_title: "Sanskrit",
+      thumbnail_url:
+        "https://i.pinimg.com/originals/15/d8/dc/15d8dcfa774d457327e656d5629f0702.jpg"
+    },
+    {
+      lookup_key: "bengali",
+      title: "Bengali",
+      sub_title: "Bengali",
+      thumbnail_url:
+        "https://i.pinimg.com/originals/ae/3d/e7/ae3de70cec5dfbd06b6402d78b795f78.jpg"
+    }
+  ];
